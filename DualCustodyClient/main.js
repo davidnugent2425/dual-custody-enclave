a7dac5d59ac74d7658b6cade4d490c758a77c650
davidnugent2425 <david.nugent2425@gmail.com>
2023-11-09 15:50:15 +0000

    main.js refactor

--- DualCustodyClient/main.js
+++ DualCustodyClient/main.js
@@ -1,212 +1,187 @@
-const { app, BrowserWindow, ipcMain, protocol, shell } = require('electron');  // Import protocol
+const { app, BrowserWindow, ipcMain, shell } = require('electron');
 const Evervault = require('@evervault/sdk');
-const evervault = new Evervault('app_hello', 'donal', { curve: 'prime256v1'});
+const evervault = new Evervault('app_hello', 'donal', { curve: 'prime256v1' });
 const log = require('electron-log');
 const { v4: uuidv4 } = require('uuid');
-const url = require('url');
+global.isCagesConfigured = false; // flag to track if cages have been configured
 
-let win;  // Make win a global variable
+let mainWindow;
 
-function createWindow() {
-    win = new BrowserWindow({
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
         width: 1000,
         height: 800,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
-            preload: __dirname + '/preload.js'
+            preload: `${__dirname}/preload.js`
         }
     });
 
-    // win.loadFile('index.html');
-    win.loadFile('login.html');
-    win.maximize();
+    mainWindow.loadFile('login.html');
+    mainWindow.maximize();
 }
 
 function initiateLogin() {
-    const auth0Domain = 'https://dev-s3fbj5e02s08godo.us.auth0.com';
-    const clientId = 'Bxm4IrLptD04TXxs9xt3JJC3TDxQ860b';
-    const redirectUri = encodeURIComponent('dual-custody://callback');
-    const responseType = 'token id_token';
-    const audience = 'https://dual-custody-backend.davidnugent2425.repl.co'
-    const scope = 'openid';
-    const nonce = uuidv4();
-
-    const authUrl = `${auth0Domain}/authorize?response_type=${responseType}&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&audience=${audience}&nonce=${nonce}`;
-
-    // This opens the URL in the user's default web browser.
+    const authUrl = 'https://dev-s3fbj5e02s08godo.us.auth0.com/authorize?' +
+        'response_type=token%20id_token&' +
+        `client_id=Bxm4IrLptD04TXxs9xt3JJC3TDxQ860b&` +
+        `redirect_uri=${encodeURIComponent('dual-custody://callback')}&` +
+        'scope=openid&' +
+        `audience=https://dual-custody-backend.davidnugent2425.repl.co&` +
+        `nonce=${uuidv4()}`;
     shell.openExternal(authUrl);
 }
+
+function storeTokens(accessToken, idToken) {
+    global.tokens = { accessToken, idToken };
+}
+
+async function makeApiRequest(url, method, headers, body = null) {
+    try {
+        const response = await fetch(url, {
+            method,
+            headers,
+            ...(body && { body: JSON.stringify(body) })
+        });
+        if (!response.ok) {
+            throw new Error(`Failed to make API request: ${response.statusText}`);
+        }
+        return response.json();
+    } catch (error) {
+        log.error('Error during API request:', error);
+        throw error;
+    }
+}
+
+function validateAccessToken() {
+    const accessToken = global.tokens.accessToken;
+    if (!accessToken) {
+        throw new Error('Access token is not available.');
+    }
+    return accessToken;
+}
+
+async function generateWalletToken() {
+    const accessToken = validateAccessToken();
+    const url = 'https://dual-custody-backend.davidnugent2425.repl.co/get-token/generate-wallet';
+    const headers = { 'Authorization': `Bearer ${accessToken}` };
+
+    return makeApiRequest(url, 'GET', headers).then(data => data.jwt);
+}
+
+async function generateTransactionToken(fromAccount, toAccount, amount) {
+    const accessToken = validateAccessToken();
+    const url = 'https://dual-custody-backend.davidnugent2425.repl.co/get-token/transaction';
+    const headers = {
+        'Authorization': `Bearer ${accessToken}`,
+        'Content-Type': 'application/json',
+    };
+    const body = { from_account: fromAccount, to_account: toAccount, amount: amount };
+
+    return makeApiRequest(url, 'POST', headers, body).then(data => data.jwt);
+}
+
+function handleAuthCallback(urlStr) {
+    const hash = new URL(urlStr).hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const accessToken = params.get('access_token');
+    const idToken = params.get('id_token');
+
+    if (accessToken && idToken) {
+        storeTokens(accessToken, idToken);
+        mainWindow.loadFile('index.html');
+    } else {
+        log.error('Failed to retrieve tokens');
+    }
+}
+
+async function configureCagesBeta(pcrValues) {
+    const requiredPcrKeys = ['PCR0', 'PCR1', 'PCR2', 'PCR8'];
+
+    // Validate that all required PCR keys are present
+    for (const key of requiredPcrKeys) {
+        if (!pcrValues.hasOwnProperty(key)) {
+            throw new Error(`Missing required PCR value: ${key}`);
+        }
+    }
+
+    // Explicitly set the necessary PCR values
+    await evervault.enableCagesBeta({
+        'dual-custody-cage': {
+            pcr0: pcrValues.PCR0,
+            pcr1: pcrValues.PCR1,
+            pcr2: pcrValues.PCR2,
+            pcr8: pcrValues.PCR8
+        }
+    });
+
+    global.isCagesConfigured = true; // Set flag to indicate cages are configured
+}
+
+// Electron app event handlers
+app.on('ready', createMainWindow);
 
 app.on('open-url', (event, urlStr) => {
     event.preventDefault();
     if (urlStr.includes('dual-custody://callback')) {
-        const hash = url.parse(urlStr).hash.substring(1);  // Remove leading '#'
-        const params = new URLSearchParams(hash);
-        const accessToken = params.get('access_token');
-        const idToken = params.get('id_token');
-
-        if (accessToken && idToken) {
-            storeTokens(accessToken, idToken);
-            win.loadFile('index.html');
-        } else {
-            log.error('Failed to retrieve tokens');
-        }
+        handleAuthCallback(urlStr);
     }
 });
 
-global.tokens = {
-    accessToken: null,
-    idToken: null
-};
-
-function storeTokens(accessToken, idToken) {
-    global.tokens.accessToken = accessToken;
-    global.tokens.idToken = idToken;
-}
-
-global.pcrs = {};
-
-ipcMain.on('store-pcrs', (event, pcrValues) => {
-    log.info('storing pcrs')
-    global.pcrs = pcrValues;
-});
-
-async function generateWalletToken() {
-    const accessToken = global.tokens.accessToken;  // assuming tokens are stored in global object as shown before
-  
-    if (!accessToken) {
-      log.error('Access token is not available.');
-      return null;
-    }
-  
-    const response = await fetch('https://dual-custody-backend.davidnugent2425.repl.co/get-token/generate-wallet', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${accessToken}`,
-      },
-    });
-  
-    if (!response.ok) {
-      log.error(`Failed to retrieve wallet token: ${response.statusText}`);
-      return null;
-    }
-  
-    const data = await response.json();
-    return data.jwt;
-}
-
-async function generateTransactionToken(fromAccount, toAccount, amount) {
-    const accessToken = global.tokens.accessToken; // assuming tokens are stored in global object as shown before
-
-    if (!accessToken) {
-        console.error('Access token is not available.');
-        return null;
-    }
-
-    if (!fromAccount || !toAccount) {
-        console.error('From account or to account is not provided.');
-        return null;
-    }
-
-    const response = await fetch('https://dual-custody-backend.davidnugent2425.repl.co/get-token/transaction', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            from_account: fromAccount,
-            to_account: toAccount,
-            amount: amount
-        }),
-    });
-
-    if (!response.ok) {
-        console.error(`Failed to retrieve transaction token: ${response.statusText}`);
-        return null;
-    }
-
-    const data = await response.json();
-    return data.jwt;
-}
-
-app.whenReady().then(() => {
-    createWindow();
-});
-
-ipcMain.on('initiate-login', () => {
-    initiateLogin();
-});
+ipcMain.on('initiate-login', initiateLogin);
 
 ipcMain.on('open-external', (event, url) => {
     shell.openExternal(url);
 });
 
-ipcMain.handle('generate-wallet', async (event) => {
+ipcMain.on('store-pcrs', async (event, pcrValues) => {
     try {
+        log.info('Storing PCRs and configuring cages');
+        await configureCagesBeta(pcrValues);
+    } catch (error) {
+        log.error('Failed to configure cages with PCRs:', error);
+    }
+});
+
+ipcMain.handle('generate-wallet', async () => {
+    try {
+        if (!global.isCagesConfigured) {
+            throw new Error('Cages have not been configured yet. Please store PCR values first.');
+        }
         const cageToken = await generateWalletToken();
 
-        await evervault.enableCagesBeta({'dual-custody': {
-            pcr0: global.pcrs.PCR0,
-            pcr1: global.pcrs.PCR1,
-            pcr2: global.pcrs.PCR2,
-            pcr8: global.pcrs.PCR8
-        }});
-        log.info('sending generate wallet request to cage', cageToken);
+        log.info('Sending generate wallet request to cage', cageToken);
 
-        const response = await fetch('https://dual-custody-cage.app-80eeb9f27e5b.cage.evervault.com/generate_wallet', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${cageToken}`,
-            },
-        });
+        const walletUrl = 'https://dual-custody-cage.app-80eeb9f27e5b.cages.evervault.com/generate_wallet';
+        const walletHeaders = { 'Authorization': `Bearer ${cageToken}` };
 
-        log.info('response received')
-
-        if (!response.ok) {
-            throw new Error(`Network response was not ok: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        return data;
+        const walletResponse = await makeApiRequest(walletUrl, 'GET', walletHeaders);
+        return walletResponse;
     } catch (error) {
-        log.error('Error:', error);
+        log.error('Error in generate-wallet handler:', error);
         throw error;
     }
 });
 
 ipcMain.handle('sign-transaction', async (event, { base64Part, fromAccount, toAccount, amount }) => {
-    await evervault.enableCagesBeta({'dual-custody': {
-        pcr0: global.pcrs.PCR0,
-        pcr1: global.pcrs.PCR1,
-        pcr2: global.pcrs.PCR2,
-        pcr8: global.pcrs.PCR8
-    }});
-    const url = 'https://dual-custody-cage.app-80eeb9f27e5b.cage.evervault.com/sign_transaction';
-    const body = JSON.stringify({ base64_part: base64Part });
-
     try {
-        const cageToken = await generateTransactionToken(fromAccount, toAccount, amount);
-        log.info('sending transaction request to cage', cageToken);
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${cageToken}`,
-            },
-            body,
-        });
-        log.info('transaction response received')
-        
-        if (!response.ok) {
-            throw new Error('Failed to sign transaction');
+        if (!global.isCagesConfigured) {
+            throw new Error('Cages have not been configured yet. Please store PCR values first.');
         }
 
-        const data = await response.json();
-        return data;
+        const cageToken = await generateTransactionToken(fromAccount, toAccount, amount);
+        log.info('Sending transaction request to cage', cageToken);
+
+        const transactionUrl = 'https://dual-custody-cage.app-80eeb9f27e5b.cages.evervault.com/sign_transaction';
+        const transactionHeaders = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${cageToken}`
+        };
+        const transactionResponse = await makeApiRequest(transactionUrl, 'POST', transactionHeaders, { base64_part: base64Part });
+        return transactionResponse;
     } catch (error) {
-        log.error('Error:', error);
+        log.error('Error in sign-transaction handler:', error);
         throw error;
     }
 });
